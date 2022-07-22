edb0e6a0219d549b40b78c2de7aad37a7b6cfaed
Nihilol <Oliverliebe@hotmail.com>
2022-07-22 16:13:57 +0200

    Commit

--- image_processing.jl
+++ image_processing.jl
@@ -18,3 +18,58 @@ using TikzPictures
 using CurveFit
 using TexTables
 using Images
+
+##
+
+function picking_files()
+
+    dataframe_path = raw"C:\Users\liebeoli\Desktop\Functional Cellulose-lignin-coating on Porous Materials\Photos";
+
+    files = open_dialog("Chose a file", GtkNullContainer(), String["*.jpg"], select_multiple=true)
+
+    entries = readdir(dataframe_path, join=true)
+
+    for i in range(1, length(entries))
+        entries[i] = split(entries[i], raw"\\")[7][1:end-4]
+    end
+
+    solvent = String[]
+
+    producer_material = String[]
+
+    iterate_files = []
+
+    pulses = []
+
+    for file_pick in files
+        push!(solvent, split(file_pick, raw"_")[1][end-3:end])
+        push!(producer_material, split(file_pick, raw"_")[2])
+        push!(pulses, parse(Int64, split(file_pick, raw"_")[3][1:end-1]))
+        push!(iterate_files, split(file_pick, raw"_")[1])
+
+    end
+
+    sort(pulses, rev = true)
+    
+    images = []
+
+    for file in files
+        push!(images, load(file))
+    end
+
+    # imshow(images[1])
+
+    for image in images
+        for i in range(1, size(image)[1])
+            for l in range(1, size(image)[2])
+                println(image[i, l])
+            end
+        end
+    end
+
+    return solvent, producer_material, pulses, entries
+end
+
+##
+
+picking_files();
