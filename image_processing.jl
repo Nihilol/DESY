91df57c3d55ed335387d9cd5d2847c4ff32b28ec
Nihilol <Oliverliebe@hotmail.com>
2022-07-25 17:43:18 +0200

    Commit

--- image_processing.jl
+++ image_processing.jl
@@ -23,6 +23,26 @@ using Images
 
 function picking_files()
 
+    solvent = String[]
+
+    producer_material = String[]
+
+    iterate_files = []
+
+    pulses = []
+
+    down_dimension = 10
+
+    length_of_colourscheme= 16000
+
+    colours_init_blue = zeros(RGB{Float64}, down_dimension, length_of_colourscheme)
+
+    colours_init_red = zeros(RGB{Float64}, down_dimension, length_of_colourscheme)
+
+    colours_init_green = zeros(RGB{Float64}, down_dimension, length_of_colourscheme)
+
+    seperation_of_copies = true
+
     # dataframe_path = raw"C:\Users\olive\Desktop\Uni\Summer Programs\DESY\Photos"
 
     dataframe_path = raw"C:\Users\liebeoli\Desktop\Functional Cellulose-lignin-coating on Porous Materials\Photos";
@@ -34,14 +54,6 @@ function picking_files()
     for i in range(1, length(entries))
         entries[i] = split(entries[i], raw"\\")[7][1:end-4]
     end
-
-    solvent = String[]
-
-    producer_material = String[]
-
-    iterate_files = []
-
-    pulses = []
 
     for file_pick in files
         push!(solvent, split(file_pick, raw"_")[1][end-3:end])
@@ -59,22 +71,12 @@ function picking_files()
         push!(images, load(file))
     end
 
-    down_dimension = 10
+    global b = 1
+    global r = 1
+    global g = 1
 
-    length_of_colourscheme= 16000
-
-    colours_init_blue = zeros(RGB{Float64}, down_dimension, length_of_colourscheme)
-
-    colours_init_red = zeros(RGB{Float64}, down_dimension, length_of_colourscheme)
-
-    colours_init_green = zeros(RGB{Float64}, down_dimension, length_of_colourscheme)
-
-    
 
     for image in images
-        global b = 1
-        global r = 1
-        global g = 1
         for i in (1:1:size(image)[1])
             for l in (1:1:size(image)[2])
                 if float(red(image[i, l])) < 0.5 && float(green(image[i, l])) < 0.5 && float(blue(image[i, l])) > 0.6
@@ -102,17 +104,7 @@ function picking_files()
         global g = g
     end
 
-    println(b - 1, " Is the amount of blue. ", r - 1, " Is the amount of red. ",  g - 1, " Is the amount of green")
-
-    # sch = RGB{Float64}(0.0, 0.0, 0.0)
-
-    # indexArray = findall(x -> x == sch, colours_init_blue)
-
-    # for i in indexArray
-    #     println("There are ", i[2], " blue pixels in the photo")
-    #     global g = i[2]
-    #     break
-    # end
+    println(b - 1, " Is the amount of blue pixels. ", r - 1, " Is the amount of red pixels. ",  g - 1, " Is the amount of green pixels.")
 
     colours_blue = zeros(RGB{Float64}, down_dimension, b - 1)
     colours_red = zeros(RGB{Float64}, down_dimension, r - 1)
@@ -134,20 +126,99 @@ function picking_files()
         end
     end
 
-    if colours_blue != empty
-        imshow(colours_blue)
+    if seperation_of_copies == true
+        global blue_seperator = b
+        global red_seperator = r
+        global green_seperator = g
+        if colours_blue != empty
+            for i in range(1, b - 1)
+                for l in range(i + 1, b - 1)
+                    if float(green(colours_blue[1, i])) - 0.00001 < float(green(colours_blue[1, l])) < float(green(colours_blue[1, i])) + 0.00001 && 
+                        float(red(colours_blue[1, i])) - 0.00001 < float(red(colours_blue[1, l])) < float(red(colours_blue[1, i])) + 0.00001 &&
+                        float(blue(colours_blue[1, i])) - 0.00001 < float(blue(colours_blue[1, l])) < float(blue(colours_blue[1, i])) + 0.00001
+                        for t in range(1, 10)
+                            colours_blue[t, l] = RGB{Float64}(0.0, 0.0, 0.0)
+                        end
+                        blue_seperator -= 1
+                        println(blue_seperator)
+                    end
+                end
+            end
+            search = RGB{Float64}(0.0, 0.0, 0.0)
+            indexArray = findall(x -> x )
+            imshow(colours_blue)
+            println(blue_seperator, " Is the corrected colour-scheme for blue.")
+            colour_seperated_blue = zeros(RGB{Float64}, down_dimension, blue_seperator)
+            w = 1
+            for i in range(1, b - 1)
+                if colours_blue[1, i] != RGB{Float64}(0.0, 0.0, 0.0)
+                    for p in range(1, 10)
+                        colour_seperated_blue[p, w] = colours_blue[1, i]
+                    end
+                    w += 1
+                end
+            end
+            imshow(colour_seperated_blue)
+        else
+            println("There are no blue pixels in this photo")
+        end
+        if colours_red != empty
+            for i in range(1, r - 1)
+                for l in range(i + 1, r - 1)
+                    if float(green(colours_red[1, i])) - 0.0001 < float(green(colours_red[1, l])) < float(green(colours_red[1, i])) + 0.0001 && 
+                        float(red(colours_red[1, i])) - 0.0001 < float(red(colours_red[1, l])) < float(red(colours_red[1, i])) + 0.0001 &&
+                        float(blue(colours_red[1, i])) - 0.0001 < float(blue(colours_red[1, l])) < float(blue(colours_red[1, i])) + 0.0001
+                        for t in range(1, 10)
+                            colours_red[t, l] = RGB{Float64}(0.0, 0.0, 0.0)
+                        end
+                        red_seperator -= 1
+                        println(red_seperator)
+                    end
+                end
+            end
+            println(red_seperator, " Is the corrected colour-scheme for red.")
+            colour_seperated_red = zeros(RGB{Float64}, down_dimension, red_seperator)
+            q = 1
+            for i in range(1, r - 1)
+                if colours_red[1, i] != RGB{Float64}(0.0, 0.0, 0.0)
+                    for p in range(1, 10)
+                        colour_seperated_red[p, q] = colours_red[1, i]
+                    end
+                    q += 1
+                end
+            end
+            imshow(colour_seperated_red)
+        else
+            println("There are no green pixels in this photo")
+        end
+        if colours_green != empty
+            println(length(colours_green[1]))
+            for i in range(1, g - 1)
+                for l in range(i + 1, g - 1)
+                    if float(green(colours_green[1, i])) - 0.00001 < float(green(colours_green[1, l])) < float(green(colours_green[1, i])) + 0.00001 && 
+                        float(red(colours_green[1, i])) - 0.00001 < float(red(colours_green[1, l])) < float(red(colours_green[1, i])) + 0.00001 &&
+                        float(blue(colours_green[1, i])) - 0.00001 < float(blue(colours_green[1, l])) < float(blue(colours_green[1, i])) + 0.00001
+                        for t in range(1, 10)
+                            colours_green[t, l] = RGB{Float64}(0.0, 0.0, 0.0)
+                        end
+                        green_seperator -= 1
+                    end
+                end
+            end
+            imshow(colours_green)
+        else
+            println("There are no green pixels in this photo")
+        end
     else
-        println("There are no blue pixels in this photo")
-    end
-    if colours_red != empty
-        imshow(colours_red)
-    else
-        println("There are no green pixels in this photo")
-    end
-    if colours_green != empty
-        imshow(colours_green)
-    else
-        println("There are no green pixels in this photo")
+        if colours_green != empty
+            imshow(colours_green)
+        end
+        if colours_red != empty
+            imshow(colours_red)
+        end
+        if colours_blue != empty
+            imshow(colours_blue)
+        end
     end
     return solvent, producer_material, pulses, entries
 end
