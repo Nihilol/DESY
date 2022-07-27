d0653285dd6b9175333f163f15b9123d5822b0cf
Nihilol <Oliverliebe@hotmail.com>
2022-07-27 13:38:05 +0200

    Commit

--- Testing.jl
+++ Testing.jl
@@ -1,12 +1,8 @@
-using GLMakie
-using DataFrames
-using Symbolics
-using StatsBase
-using CSV
-using StatsModels
-using AbstractPlotting
-using ImageView
-using RandomNumbers
-using TikzPictures
-using CurveFit
-using TexTables
+using GLMakie: Figure
+
+
+
+
+
+
+fig1 = Figure()
